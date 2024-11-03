4381be7b6f325a1a8b2bdf2111ed12712c826b84
srikeerthis <srikeerthisrinivasan@gmail.com>
2024-11-03 10:58:39 -0600

    feat: created a get message service

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var express = require("express");
 app = express();
 
 app.use("/", express.static(__dirname));
+
+// varaible to store messages
+messages = [
+  { name: "Tim", message: "hi" },
+  { name: "Cook", message: "Hello" },
+];
+// route to recieve messages
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
 var server = app.listen(3000, () => {
   console.log("server listening on ", server.address().port);
 });
